5ee00f60bde3a2146f87b0c619ef6b2254b7b2ea
sudiptasoumyajit <84839625+sudiptasoumyajit@users.noreply.github.com>
2021-12-16 17:01:45 +0530

    DJ o DJ Haha

--- main.js
+++ main.js
@@ -1,9 +1,18 @@
 song = "";
+leftWristX = 0;
+leftWristY = 0;
+rightWristX = 0;
+rightWristY = 0;
 function setup() {
     canvas = createCanvas(600, 500);
     canvas.center();
     video = createCapture(VIDEO);
     video.hide();
+    poseNet = ml5.poseNet(video, modelLoaded);
+    poseNet.on('pose', gotPoses);
+}
+function modelLoaded() {
+    console.log("Debug: pose.net has been initialised. Line-10");
 }
 function draw() {
     image(video, 0, 0, 600, 500);
@@ -13,4 +22,19 @@ function preload() {
 }
 function play() {
     song.play();
+    song.setVolume(1);
+    song.rate(2);
+}
+function gotPoses(results) {
+    if (results.length > 0) {
+        console.log(results);
+        leftWristX = results[0].pose.leftWrist.x;
+        leftWristY = results[0].pose.leftWrist.y;
+        console.log("left wrist x ="+leftWristX);
+        console.log("left wrist y ="+leftWristY);
+        rightWristX = results[0].pose.rightWrist.x;
+        rightWristY = results[0].pose.rightWrist.y;
+        console.log("right wrist x ="+rightWristX);
+        console.log("right wrist y ="+rightWristY);
+    }
 }
