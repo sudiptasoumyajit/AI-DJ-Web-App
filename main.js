36b84646b7f03cd894d5615355f15bed813e03f6
sudiptasoumyajit <84839625+sudiptasoumyajit@users.noreply.github.com>
2021-12-18 14:47:10 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -15,7 +15,17 @@ function modelLoaded() {
     console.log("Debug: pose.net has been initialised. Line-10");
 }
 function draw() {
+    fill("#FF0000");
+    stroke("#FF0000");
     image(video, 0, 0, 600, 500);
+    if(scoreLeftWrist > 0.2) {
+        circle(leftWristX, leftWristY, 0);
+        InNumberleftWristY = Number(leftWristY);
+        remove_decimals = floor(InNumberleftWristY);
+        volume = remove_decimals/500;
+        document.getElementById("volume").innerHTML="volume = "+volume;
+        song.setVolume(volume);
+    }
 }
 function preload() {
     song = loadSound("music.mp3");
@@ -28,6 +38,8 @@ function play() {
 function gotPoses(results) {
     if (results.length > 0) {
         console.log(results);
+        scoreLeftWrist = results[0].pose.keypoints[9].score;
+        console.log("scoreLeftWrist = " + scoreLeftWrist);
         leftWristX = results[0].pose.leftWrist.x;
         leftWristY = results[0].pose.leftWrist.y;
         console.log("left wrist x ="+leftWristX);
